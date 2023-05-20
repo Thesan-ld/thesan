1d08568d0032ba106f92e956ed6128abb93314af
Frank Johnson <frankjohnson1993@gmail.com>
2023-05-20 14:33:24 -0400

    Add curtain effect to project carousel

--- src/lib/components/ProjectCarousel.svelte
+++ src/lib/components/ProjectCarousel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
 	import type { ExpandedProject, ExpandedProjectCarousel } from '$lib/sanity';
 	import ProjectCard from './ProjectCard.svelte';
 	export let data: ExpandedProjectCarousel;
@@ -7,35 +8,29 @@
 
 	let listItems = [] as HTMLLIElement[];
 	let currentIndex = 0;
-	$: nextIndex = currentIndex === data.projects.length - 1 ? 0 : currentIndex + 1;
-	$: previousIndex = currentIndex === 0 ? data.projects.length - 1 : currentIndex - 1;
-	$: currentImage = listItems[currentIndex];
+	let previousIndex = 0;
+	$: forwardIndex = currentIndex === data.projects.length - 1 ? 0 : currentIndex + 1;
+	$: backwardIndex = currentIndex === 0 ? data.projects.length - 1 : currentIndex - 1;
+	$: shouldMoveFromLeft = !(previousIndex === currentIndex - 1
+		|| (currentIndex === 0 && previousIndex === data.projects.length - 1));
 	const incrementIndex = () => {
+		previousIndex = currentIndex;
 		currentIndex = currentIndex === listItems.length - 1 ? 0 : currentIndex + 1;
 	};
 	const decrementIndex = () => {
+		previousIndex = currentIndex;
 		currentIndex = currentIndex === 0 ? listItems.length - 1 : currentIndex - 1;
 	};
 
-	// function updateScroll(el: HTMLLIElement) {
-	//     el?.scrollIntoView({
-	//         behavior: 'smooth',
-	//         block: 'nearest',
-	//         inline: 'center',
-	//     })
-	// }
+	const intervalLength = 5000
+	let interval = setInterval(() => {
+	    incrementIndex()
+	}, intervalLength)
 
-	// $: updateScroll(currentImage)
-
-	// const intervalLength = 5000
-	// let interval = setInterval(() => {
-	//     incrementIndex()
-	// }, intervalLength)
-
-	// function resetInterval() {
-	//     clearInterval(interval)
-	//     interval = setInterval(() => incrementIndex(), intervalLength)
-	// }
+	function resetInterval() {
+	    clearInterval(interval)
+	    interval = setInterval(() => incrementIndex(), intervalLength)
+	}
 </script>
 
 <section>
@@ -44,13 +39,19 @@
 			class="step-btn"
 			on:click={() => {
 				decrementIndex();
-				// resetInterval();
-			}}>Previous</button
-		>
+				resetInterval();
+			}}>
+			<div class="text-wrapper">
+				<span>Previous</span>
+			</div>
+			<div class="img-wrapper">
+				<img src={data?.projects[backwardIndex]?.coverImage?.url} alt="" />
+			</div>
+		</button>
 	{/if}
 	<ul class="stage">
 		{#each data?.projects || [] as project, index (project?._id)}
-			<li bind:this={listItems[index]} class:active={currentIndex == index}>
+			<li bind:this={listItems[index]} class:active={currentIndex == index} class:fromLeft={shouldMoveFromLeft}>
 				<ProjectCard {project} />
 			</li>
 		{/each}
@@ -60,11 +61,15 @@
 			class="step-btn"
 			on:click={() => {
 				incrementIndex();
-				// resetInterval();
+				resetInterval();
 			}}
 		>
-			Next
-			<!-- <img src={data?.projects[nextIndex]?.mainImage?.asset?.url} alt="" /> -->
+			<div class="text-wrapper">
+				<span>Next</span>
+			</div>
+			<div class="img-wrapper">
+				<img src={data?.projects[forwardIndex]?.coverImage?.url} alt="" />
+			</div>
 		</button>
 	{/if}
 	<ul class="dots" aria-hidden="true">
@@ -74,7 +79,7 @@
 					class:active={index === currentIndex}
 					on:click={() => {
 						currentIndex = index;
-						// resetInterval();
+						resetInterval();
 					}}
 				/>
 			</li>
@@ -123,7 +128,24 @@
 	}
 
 	.stage li.active {
+		--dir: 1; /* from the right by default */
 		@apply not-sr-only;
+		animation: slide .3s ease-out forwards;
+	}
+
+	.stage li.fromLeft {
+		--dir: -1; /* from the left */
+	}
+
+	@keyframes slide {
+		from {
+			opacity: 0;
+			transform: translateX(calc(var(--dir) * 10px));
+		}
+		to {
+			opacity: 1;
+			transform: translateX(0);
+		}
 	}
 
 	.stage li > :global(a) {
@@ -137,15 +159,7 @@
         height: 100%;
     }
 
-	button {
-		@apply bg-white text-black;
-		border: 1px solid black;
-		padding: 0.5rem 1rem;
-		border-radius: 0.5rem;
-		font-size: 1rem;
-		height: fit-content;
-		font-weight: bold;
-		cursor: pointer;
+	.step-btn {
 		--outline: black;
         --thickness: 1px;
 		text-shadow:
@@ -153,41 +167,95 @@
             var(--thickness) calc(-1 * var(--thickness)) 0 var(--outline),
             calc(-1 * var(--thickness)) var(--thickness) 0 var(--outline),
 			var(--thickness) var(--thickness) 0 var(--outline);
-	}
-
-	button.step-btn {
-		--inset: 40px;
+		--inset: -5px;
+		--swing: 12deg;
 		position: absolute;
-		top: 50%;
+		top: 0;
+		bottom: 0;
+		width: 180px;
 		left: var(--inset);
 		z-index: 1;
-		color: white;
+		@apply text-zinc-400 hover:text-zinc-50;
 		letter-spacing: 0.1ch;
 		font-family: 'Poppins', sans-serif;
+		font-weight: 200;
 		background: transparent;
 		border: none;
 		font-size: 2rem;
 		transform-origin: center;
 		text-align: center;
-		transform: translate(-50%, -50%) rotate(90deg);
+	}
+
+	.text-wrapper {
+		position: absolute;
+		inset: 0;
+		display: grid;
+		align-content: center;
+		justify-content: flex-start;
+		transform-origin: top left;
+		transform: rotate(0);
+		transition: transform .11s ease-out;
+	}
+
+	.text-wrapper span {
+		transform: rotate(90deg);
+	}
+
+	.step-btn:last-of-type .text-wrapper span {
+		transform: rotate(-90deg);
+	}
+
+	.step-btn:last-of-type .text-wrapper {
+		transform-origin: top right;
+		justify-content: flex-end;
+	}
+
+	.step-btn:hover .text-wrapper {
+		transform: rotate(calc(-1 * var(--swing)));
+	}
+
+	.step-btn:last-of-type:hover .text-wrapper {
+		transform: rotate(var(--swing));
+	}
+
+	.step-btn .img-wrapper {
+		position: absolute;
+		top: 0;
+		z-index: -1;
+		@apply h-full w-full;
+		clip-path: polygon(0 0, 0 100%, 0 100%);
+		transition: clip-path .11s ease-out;
+	}
+
+	.step-btn:last-of-type .img-wrapper {
+		clip-path: polygon(100% 0, 100% 100%, 100% 100%);
+	}
+
+	.step-btn img {
+		@apply h-full w-full object-cover;
+		object-position: center;
 	}
 
 	button.step-btn:last-of-type {
 		left: auto;
 		right: var(--inset);
-		transform: translate(50%, -50%) rotate(-90deg);
+	}
+
+	.step-btn:hover .img-wrapper {
+		clip-path: polygon(0 0, 100% 100%, 0 100%);
+	}
+
+	.step-btn:last-of-type:hover .img-wrapper {
+		clip-path: polygon(100% 0, 100% 100%, 0 100%);
 	}
 
 	.dots button {
-		@apply w-4 h-4 rounded-full;
+		@apply w-3 h-3 rounded-full;
 		display: block;
 		padding: 0;
 		margin: 0;
-		@apply border-slate-400 bg-slate-800;
-	}
-
-	button:hover {
-		@apply bg-black text-white;
+		border: solid 1px;
+		@apply border-slate-400 bg-zinc-900;
 	}
 
 	.dots button:hover {
